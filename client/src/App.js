1b866f79a748400180aab7c99992979e2afdc906
RJD02 <dulangeraviraj@gmail.com>
2022-07-20 02:00:57 +0530

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Record from "./components/Record";
+import Graph from "./pages/Visual/Graph";
 
 function App() {
   // MQ2, DHT, Level
   return (
     <div className="App">
-      <Record />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Record />} />
+          <Route path="/visual" element={<Graph />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
