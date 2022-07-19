79dc905a0420bbaf1b351fbacbaaaec22c05e421
RJD02 <dulangeraviraj@gmail.com>
2022-07-20 00:22:54 +0530

    Render app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Record from "./components/Record";
+
+function App() {
+  // MQ2, DHT, Level
+  return (
+    <div className="App">
+      <Record />
+    </div>
+  );
+}
+
+export default App;
